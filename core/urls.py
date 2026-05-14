e7b6e8488bec2a04d85e054b6f840da7c839e791
Yoavtrejo <yoavzipacnat@gmail.com>
2026-05-14 13:21:35 -0600

    fix: update settings and urls

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+from materials.views import MaterialViewSet
+from rest_framework.routers import DefaultRouter
+default_router = DefaultRouter()
+default_router.register(r'materiales', MaterialViewSet, basename='material')
 urlpatterns = [
+    path('api/', include(default_router.urls)),
     path('admin/', admin.site.urls),
 ]
